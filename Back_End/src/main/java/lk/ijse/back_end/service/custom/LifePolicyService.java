a2dd4f465fcc43cbdf90522f836cb648ab1b7fd2
Pasindu Madushan <pasindu24hh@gmail.com>
2026-03-30 23:52:01 +0530

    error fix

--- Back_End/src/main/java/lk/ijse/back_end/service/custom/LifePolicyService.java
+++ Back_End/src/main/java/lk/ijse/back_end/service/custom/LifePolicyService.java
@@ -1,5 +1,6 @@
 package lk.ijse.back_end.service.custom;
 
+import jakarta.transaction.Transactional;
 import lk.ijse.back_end.dto.LifePolicyDTO;
 import lk.ijse.back_end.entity.LifePolicy;
 
@@ -7,5 +8,9 @@ import java.util.List;
 
 public interface LifePolicyService {
     LifePolicy save(LifePolicyDTO dto);
+
+    @Transactional
+    void updateStatus(Integer id, String status);
+
     List<LifePolicy> getAll();
 }
