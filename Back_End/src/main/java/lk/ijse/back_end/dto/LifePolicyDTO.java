42ef29dc1fe4b594de58e418fc591439610a5e6b
Pasindu Madushan <pasindu24hh@gmail.com>
2026-03-30 16:33:41 +0530

    add dto

---
+++ Back_End/src/main/java/lk/ijse/back_end/dto/LifePolicyDTO.java
@@ -0,0 +1,26 @@
+package lk.ijse.back_end.dto;
+
+import lombok.Data;
+
+@Data
+public class LifePolicyDTO {
+
+    private String fullName;
+    private String nic;
+    private String gender;
+    private String dob;
+    private String email;
+    private String mobile;
+    private String address;
+
+    private double height;
+    private double weight;
+    private boolean smoker;
+    private String diseases;
+
+    private double sumAssured;
+    private int policyTerm;
+    private double monthlyPremium;
+    private String policyStart;
+    private String plan;
+}
