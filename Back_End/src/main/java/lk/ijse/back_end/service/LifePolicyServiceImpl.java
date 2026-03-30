e65f3ce5334d431b4417f684d3889d0c9b3b51c9
Pasindu Madushan <pasindu24hh@gmail.com>
2026-03-30 16:34:42 +0530

    add life serviceImpl

---
+++ Back_End/src/main/java/lk/ijse/back_end/service/LifePolicyServiceImpl.java
@@ -0,0 +1,50 @@
+package lk.ijse.back_end.service;
+
+import lk.ijse.back_end.dto.LifePolicyDTO;
+import lk.ijse.back_end.entity.LifePolicy;
+import lk.ijse.back_end.repository.LifePolicyRepository;
+import lk.ijse.back_end.service.custom.LifePolicyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class LifePolicyServiceImpl implements LifePolicyService {
+
+    private final LifePolicyRepository repo;
+
+    @Override
+    public LifePolicy save(LifePolicyDTO dto) {
+
+        LifePolicy p = new LifePolicy();
+
+        p.setFullName(dto.getFullName());
+        p.setNic(dto.getNic());
+        p.setGender(dto.getGender());
+        p.setDob(LocalDate.parse(dto.getDob()));
+        p.setEmail(dto.getEmail());
+        p.setMobile(dto.getMobile());
+        p.setAddress(dto.getAddress());
+
+        p.setHeight(dto.getHeight());
+        p.setWeight(dto.getWeight());
+        p.setSmoker(dto.isSmoker());
+        p.setDiseases(dto.getDiseases());
+
+        p.setSumAssured(dto.getSumAssured());
+        p.setPolicyTerm(dto.getPolicyTerm());
+        p.setMonthlyPremium(dto.getMonthlyPremium());
+        p.setPolicyStart(LocalDate.parse(dto.getPolicyStart()));
+        p.setPlan(dto.getPlan());
+
+        return repo.save(p);
+    }
+
+    @Override
+    public List<LifePolicy> getAll() {
+        return repo.findAll();
+    }
+}
